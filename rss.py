6b2432f30d68561e8ce7619cf0052444b5e47a5b
nikhileshp <nikhilesh.prabhakar2016@vitstudent.ac.in>
2020-05-31 22:43:05 +0530

    Update rss.py

--- rss.py
+++ rss.py
@@ -1,6 +1,6 @@
 import feedparser
 
-NewsFeed = feedparser.parse("https://www.upwork.com/ab/feed/jobs/rss?api_params=1&amp;orgUid=817028735655936002&amp;securityToken=19190f2eab51942de3094fa07e26242cf8752ef30dc8b4f04bb224d6471fcb7d14d8ad372f0cefbc6a61b530427bf6cf41eda3797456197a301757483886904d&amp;sort=recency&amp;userUid=817028735655936000")
+NewsFeed = feedparser.parse("https://www.upwork.com/ab/feed/jobs/rss?api_params=1&amp;orgUid=<UID>&amp;securityToken=<SEC_TOKEN>&amp;sort=recency&amp;userUid=<USERUID>")
 if(NewsFeed.status == 200):
 
     pub_list = NewsFeed.feed.published.split(" ")[0:-2]
